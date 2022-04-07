405a960efd84230d562b3b6dfb7903ec927edc5a
enbehzadi <en.behzadi@gmail.com>
2022-04-07 12:33:23 +0430

    remove class CounterObsever

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'events/counter_event.dart';
 import 'states/counter_state.dart';
 import 'views/counter_view_bloc.dart';
-import 'CounterObserver.dart';
 import 'blocs/counter_bloc.dart';
 void main() => runApp(CounterApp());
 class CounterApp extends StatelessWidget {
